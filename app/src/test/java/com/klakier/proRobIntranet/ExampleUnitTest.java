d2231678887c17b5a498df7e6deb3c042ffff06c
Lukasz Paleczek <lukasz.paleczek@gmail.com>
2020-08-10 21:58:25 +0200

    Adding Timesheet filter, working on FilterData class

--- app/src/test/java/com/klakier/proRobIntranet/ExampleUnitTest.java
+++ app/src/test/java/com/klakier/proRobIntranet/ExampleUnitTest.java
@@ -2,6 +2,9 @@ package com.klakier.proRobIntranet;
 
 import org.junit.Test;
 
+import java.sql.Date;
+import java.util.Calendar;
+
 import static org.junit.Assert.assertEquals;
 
 /**
@@ -13,5 +16,23 @@ public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void filterDate_isWorkCorrect() {
+        FilterData fd = new FilterData(Date.valueOf("2021-02-09"));
+        fd.setRangeWeek(53,2020);
+        fd.setRangeDateToDate(Date.valueOf("2010-04-05"), Date.valueOf("2031-05-12"));
+        fd.setRangeMonth(Calendar.APRIL,2021);
+        Date d1 = fd.getRangeStartDate();
+        Date d2 = fd.getRangeEndDate();
+
+        fd.setRangeTypeSelected(FilterData.RANGE_WEEK);
+        Date d3 = fd.getRangeStartDate();
+        Date d4 = fd.getRangeEndDate();
+
+        fd.setRangeTypeSelected(FilterData.RANGE_DATE_TO_DATE);
+        Date d5 = fd.getRangeStartDate();
+        Date d6 = fd.getRangeEndDate();
     }
 }
